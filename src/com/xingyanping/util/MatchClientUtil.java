f70b84c03945a1d03157e05110f14da1c8d07a3a
Norris Zhang <norris@intela.ai>
2017-08-14 21:26:27 +1200

    [11] no new data, no excel. multiple guodu to single file.

--- src/com/xingyanping/util/MatchClientUtil.java
+++ src/com/xingyanping/util/MatchClientUtil.java
@@ -1,12 +1,21 @@
 package com.xingyanping.util;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.xingyanping.datamodel.ClientPortRelationship;
 import com.xingyanping.datamodel.OriginalReport;
 
 public class MatchClientUtil {
 	public static final String NOT_MATCH = "NotMatch";
+	private static final Map<String, String> clientNameFilter = new HashMap<>();
+	static {
+		clientNameFilter.put("国都003627", "国都");
+		clientNameFilter.put("国都1095", "国都");
+		clientNameFilter.put("国都1100", "国都");
+		clientNameFilter.put("国都1106", "国都");
+	}
 	public static String matchClient(OriginalReport orre, List<ClientPortRelationship> cprsList) {
 		ClientPortRelationship matchedCprs = null;
 		String matchedPort = "";
@@ -26,7 +35,10 @@ public class MatchClientUtil {
 			return NOT_MATCH;
 		}
 		orre.setMatchesClientPortRelationship(matchedCprs);
-		return clientName;
+		return filterClientName(clientName);
+	}
+	private static String filterClientName(String clientName) {
+		return clientNameFilter.containsKey(clientName) ? clientNameFilter.get(clientName) : clientName;
 	}
 
 }
