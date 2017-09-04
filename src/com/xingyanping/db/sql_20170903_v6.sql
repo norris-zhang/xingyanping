28e1b3104d86553ba0c728191a55ffa7bc5c8b27
Norris Zhang <norris@intela.ai>
2017-09-04 19:28:11 +1200

    [67] sql with unique company short names.

--- src/com/xingyanping/db/sql_20170903_v6.sql
+++ src/com/xingyanping/db/sql_20170903_v6.sql
@@ -8,13 +8,13 @@ update `client_port_relationship` set `cprs_order`=4 where `cprs_company_short_n
 update `client_port_relationship` set `cprs_order`=5 where `cprs_company_short_name`='原树管理';
 update `client_port_relationship` set `cprs_order`=6 where `cprs_company_short_name`='山东网易';
 update `client_port_relationship` set `cprs_order`=7 where `cprs_company_short_name`='济南北秀';
-update `client_port_relationship` set `cprs_order`=8 where `cprs_company_short_name`='陕西创远' and `cprs_port` like '10690219%';
+update `client_port_relationship` set `cprs_order`=8 where `cprs_company_short_name`='陕西创远2' and `cprs_port` like '10690219%';
 update `client_port_relationship` set `cprs_order`=9 where `cprs_company_short_name`='杭州三石';
 update `client_port_relationship` set `cprs_order`=10 where `cprs_company_short_name`='山东航空';
 update `client_port_relationship` set `cprs_order`=11 where `cprs_company_short_name`='企业银行';
 update `client_port_relationship` set `cprs_order`=12 where `cprs_company_short_name`='济南百秀';
 update `client_port_relationship` set `cprs_order`=13 where `cprs_company_short_name`='陕西上格';
-update `client_port_relationship` set `cprs_order`=14 where `cprs_company_short_name`='陕西创远' and `cprs_port` like '106575681025%';
+update `client_port_relationship` set `cprs_order`=14 where `cprs_company_short_name`='陕西创远1' and `cprs_port` like '106575681025%';
 update `client_port_relationship` set `cprs_order`=15 where `cprs_company_short_name`='创力天行';
 update `client_port_relationship` set `cprs_order`=16 where `cprs_company_short_name`='西安锦诚';
 update `client_port_relationship` set `cprs_order`=17 where `cprs_company_short_name`='陕西世荣';
